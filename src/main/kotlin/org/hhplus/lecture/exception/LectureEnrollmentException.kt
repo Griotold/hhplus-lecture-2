07c57deac0d45fbcdf1dcf85143b407a7a93f54b
Griotold <gotjd9773@naver.com>
2024-06-30 12:31:29 +0900

    코틀린에서는 체크드 익셉션과 언체크드 익셉션의 구분이 없다.

--- src/main/kotlin/org/hhplus/lecture/exception/LectureEnrollmentException.kt
+++ src/main/kotlin/org/hhplus/lecture/exception/LectureEnrollmentException.kt
@@ -1,5 +1,6 @@
 package org.hhplus.lecture.exception
 
+// 코틀린에서는 체크드와 언체크드 예외의 구분이 없다.
 open class LectureEnrollmentException(message: String) : Exception(message) {
     val notNullMessage: String
         // 커스텀 게터 정의
